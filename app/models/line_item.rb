19bddb71eb3dc43e8c1413b2b3a2fe1f71fa4cee
c.obrien40@nuigalway.ie <c.obrien40@nuigalway.ie>
2015-10-15 18:42:31 +0100

    Fixed some linking problems

--- app/models/line_item.rb
+++ app/models/line_item.rb
@@ -1,4 +1,5 @@
 class LineItem < ActiveRecord::Base
   belongs_to :album
   belongs_to :profile
+  validates :album, uniqueness: { scope: :profile, message: "This album is already in your profile" }
 end
